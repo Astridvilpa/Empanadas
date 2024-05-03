85390de27edc0161f229bbde82588b1fb8510783
Astrid Vilora <astridviloria54@gmail.com>
2024-05-03 23:39:01 +0200

    se calcula el costo de cada tipo de empanada aplicando la oferta se aplica el redondeo de decimales y se coloca la condicion else por si no se cumple indique un error

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-// Función para calcular el precio total de las empanadas
+// Función 
 export const fn = (empanadaPollo, empanadaCarne, empanadaVegetariana) => {
 
-   // Comprobación de las condiciones
-   if (
-      empanadaPollo + empanadaCarne + empanadaVegetariana < 40 &&
-      empanadaPollo + empanadaCarne + empanadaVegetariana >= 3 &&
-      empanadaPollo >= 0 && empanadaCarne >= 0 && empanadaVegetariana >= 0 &&
-      (empanadaPollo + empanadaCarne + empanadaVegetariana) % 3 === 0
-  ) 
 
+  // condiciones
+  if (
+    empanadaPollo + empanadaCarne + empanadaVegetariana < 40 &&
+    empanadaPollo + empanadaCarne + empanadaVegetariana >= 3 &&
+    empanadaPollo >= 0 &&
+    empanadaCarne >= 0 &&
+    empanadaVegetariana >= 0 &&
+    (empanadaPollo + empanadaCarne + empanadaVegetariana) % 3 === 0
+  ) {
+   
+    // costo de cada tipo de empanada y aplicando la oferta especial
+    const precioPollo = empanadaPollo * 12;
+    const precioCarne = empanadaCarne * 14;
+    const precioVegetariana = empanadaVegetariana * 16;
+    const total = (precioPollo + precioCarne + precioVegetariana) / 3;
 
-  
-};
+    // Redondeo  precio total
+    return Math.ceil(total);
+  } else {
+    throw Error;
+  }
+};  
