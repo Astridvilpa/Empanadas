bd666f8353cae8527f2145cd91af2aa9ce77c238
Astrid Vilora <astridviloria54@gmail.com>
2024-05-03 23:24:20 +0200

    Se crea generan distintas condiciones, segun lo solicitado en el problema

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // Función para calcular el precio total de las empanadas
 export const fn = (empanadaPollo, empanadaCarne, empanadaVegetariana) => {
 
+   // Comprobación de las condiciones
+   if (
+      empanadaPollo + empanadaCarne + empanadaVegetariana < 40 &&
+      empanadaPollo + empanadaCarne + empanadaVegetariana >= 3 &&
+      empanadaPollo >= 0 && empanadaCarne >= 0 && empanadaVegetariana >= 0 &&
+      (empanadaPollo + empanadaCarne + empanadaVegetariana) % 3 === 0
+  ) 
+
+
   
 };
